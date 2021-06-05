0316365d0c1c05480ed6abea2394e67bb6c8f1cd
Viktor Kreschenski <viktor.crash@yahoo.com>
2021-06-05 16:02:42 +0200

    Enable debugging in bazel

--- textto3d/src/mainwindow.cpp
+++ textto3d/src/mainwindow.cpp
@@ -28,15 +28,16 @@ void MainWindow::resizeGL(int w, int h){
 }
 
 void MainWindow::paintGL(){
-    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_QUADS);
-
+    glColor3f(1.0f, 0.0f, 0.0f);
     glVertex2f(-0.5f, -0.5f);
+    glColor3f(0.0f, 1.0f, 0.0f);
     glVertex2f(0.5f, -0.5f);
+    glColor3f(0.0f, 0.0f, 1.0f);
     glVertex2f(0.5f, 0.5f);
+    glColor3f(0.0f, 0.5f, 0.5f);
     glVertex2f(-0.5f, 0.5f);
-
     glEnd();
     glFlush();
 }
